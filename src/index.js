04c9e9f67a6bce2883c22b5d16dbb26b9663bf20
MOHUND <103984452+mohund@users.noreply.github.com>
2024-12-03 16:19:38 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
